6f17e2d8dd4966b18cbb3af57eb0a4efc9f0396d
Filip Votek <f.vojtek01@gmail.com>
2020-12-16 16:06:52 +0100

    ukol2

---
+++ ukol 2 (16.12)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ukol_2__16._12_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(1, 10);
+            int b = rnd.Next(1, 10);
+
+            if (a > b)
+            {
+                Console.WriteLine("Cislo {0:F} je vetsi nez {1:F} " ,a,b);
+            }  
+            else if (a < b)
+            {
+                Console.WriteLine("Cislo {0:F} je mensi nez {1:F}" ,a,b);
+            }
+            else
+            {
+                Console.WriteLine("Cisla {0:F} jsou rovny {1:F} " ,a,b);
+            }
+            Console.ReadKey();
+
+           
+
+        }
+    }
+}
